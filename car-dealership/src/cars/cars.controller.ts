c9d63b190bc8842010d2a746035c3b2b1cca9962
fernando4974 <fernando4974@gmail.com>
2025-10-29 17:08:37 -0500

    dia 6

--- car-dealership/src/cars/cars.controller.ts
+++ car-dealership/src/cars/cars.controller.ts
@@ -1,27 +1,51 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { Json } from 'sequelize/lib/utils';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
-  errorP: string;
-  constructor(private readonly _carService: CarsService) {
-    this.errorP = 'Method not implemented.';
-  }
+  constructor(private readonly _carService: CarsService) {}
   @Get('/FindAll')
   getAllCars() {
-    const car = this._carService.getCars();
+    const cars = this._carService.getCars();
 
-    return car;
+    return cars;
   }
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
-    // if (parseInt(id).toString() === 'NaN') {
-    //   return console.log('Error de tipos');
-    // }
-
-    const cars = this._carService.getCarById(+id);
+  getCarById(
+    @Param('id', new ParseUUIDPipe({ version: '4', errorHttpStatusCode: 404 }))
+    id: string,
+  ) {
+    const cars = this._carService.getCarById(id);
     console.log(cars);
     return cars;
+  }
+  @Post('/create')
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return createCarDto;
+  }
+  @Patch(':id/update')
+  updateCar(@Param('id', ParseIntPipe) id: number) {
+    return `This action updates a #${id} car`;
+  }
+  @Patch(':id/update')
+  updateCarByBody(@Body() body: Json) {
+    // crear interface per al body de este endpoint
+    return body;
+  }
+  @Delete(':id/delete')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return `This action deletes a #${id} car`;
   }
 }
--- car-dealership/src/cars/cars.service.ts
+++ car-dealership/src/cars/cars.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CarsService {
-  private cars = [
-    { id: 1, brand: 'Toyota', model: 'Corolla', year: 2020 },
-    { id: 2, brand: 'Honda', model: 'Civic', year: 2019 },
-    { id: 3, brand: 'Ford', model: 'Mustang', year: 2021 },
+  private cars: Car[] = [
+    { id: uuid(), brand: 'Toyota', model: 'Corolla' },
+    { id: uuid(), brand: 'Honda', model: 'Civic' },
+    { id: uuid(), brand: 'Ford', model: 'Mustang' },
   ];
 
   public getCars() {
     return this.cars;
   }
-  public getCarById(id: number) {
-    if (id > this.cars.length || id < 0)
-      throw new NotFoundException('Id Car not found');
+  public getCarById(id: string) {
+    const car = this.cars.find((cars) => cars.id === id);
+    if (!car) throw new NotFoundException('Id Car not found');
 
-    return this.cars.find((car) => car.id === id);
+    return car;
   }
 }
